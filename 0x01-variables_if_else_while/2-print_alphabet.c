c686cf117db5e24a977635554d935d6576f7ef8f
RyanTk03 <rayanetoko@gmail.com>
2023-08-14 16:08:10 -0700

    0x02-4

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+ * The main function
+ *
+ * @param void
+ *
+ * @return Always 0
+ */
+int main(void)
+{
+    char letter = 'a';
+
+    while (letter <= 'z') {
+        putchar(letter);
+        letter++;
+    }
+
+    putchar('\n');
+
+    return 0;
+}
