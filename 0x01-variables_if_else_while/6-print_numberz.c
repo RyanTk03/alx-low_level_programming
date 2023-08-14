8fd1bed8141fbd6bf2ddf562572acfc2f3394af6
RyanTk03 <rayanetoko@gmail.com>
2023-08-14 16:58:10 -0700

    0x01-6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,16 +10,10 @@
  */
 int main(void)
 {
-	putchar('0' + 0);
-	putchar('0' + 1);
-	putchar('0' + 2);
-	putchar('0' + 3);
-	putchar('0' + 4);
-	putchar('0' + 5);
-	putchar('0' + 6);
-	putchar('0' + 7);
-	putchar('0' + 8);
-	putchar('0' + 9);
+	for(int i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+	}
 
 	putchar('\n');
 
