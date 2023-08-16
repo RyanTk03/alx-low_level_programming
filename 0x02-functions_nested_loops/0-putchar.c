02b202986dec8efa23c3a6dfddd9e9cd5ffcd8e2
RyanTk-L <tkryan03@gmail.com>
2023-08-16 11:27:07 -0700

    0x02-2

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints a message to the standard error.
+ * Return: On success 1, on error -1.
+ */
+int main(void)
+{
+	int i = 0;
+	char *str = "_putchar";
+
+	while(_putchar(str[i]))
+		i++;
+
+	_putchar('\n');
+
+	return (1);
+}
