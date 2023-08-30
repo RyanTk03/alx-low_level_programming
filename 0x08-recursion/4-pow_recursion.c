d95f2ad7769b77af59a30d89f2802535c561d0bd
RyanTk-L <tkryan03@gmail.com>
2023-08-30 10:56:40 -0700

    0x08-5

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -13,11 +13,9 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	else if (y == 0)
 		return (1);
-	else
-	{
-		if (y > 1)
-			return (x * _pow_recursion(x, y - 1));
-		else
-			return (x);
-	}
+
+	if (y > 1)
+		return (x * _pow_recursion(x, y - 1));
+
+	return (x);
 }
