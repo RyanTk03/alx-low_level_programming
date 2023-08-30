fdb6c66ee20d4c37725f5fcb8744b205261b44d0
RyanTk-L <tkryan03@gmail.com>
2023-08-30 08:51:58 -0700

    0x08-1

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,6 +12,6 @@ void _puts_recursion(char *s)
 	s++;
 	
 	if (*s)
-		_puts_recursion(s)
+		_puts_recursion(s);
 }
 
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Write a function that prints a string in reverse.
+ * @s: The string to show
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		s++;
+		_print_rev_recursion(s);
+	}
+
+	s--;
+	_putchar(*s);
+}
