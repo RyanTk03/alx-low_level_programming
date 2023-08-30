267ffe379a616fb85f61ed4bc244f142b694cfa5
RyanTk-L <tkryan03@gmail.com>
2023-08-30 14:53:31 -0700

    0x08-10

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,52 +1,5 @@
 #include "main.h"
 
-/**
- * cmp - compare two string.
- * @s1: The string 1.
- * @s2: The string 2.
- * @w: define if a wild char is active or not.
- *
- * Return: 1 if they are same and 0 else.
- */
-int cmp(char *s1, char *s2, int w)
-{
-if (!*s1)
-{
-if (!*s2)
-return (1);
-else
-{
-if (w)
-return (1);
-else
-return (0);
-}
-}
-
-if (!*s2 && w)
-return (1);
-else if (!*s2 && *s1)
-return (0);
-
-if (*s2 != '*')
-{
-if (*s1 == *s2)
-{
-if (w)
-w = 0;
-return (cmp(s1 + 1, s2 + 1, w));
-}
-if (w)
-return (cmp(s1 + 1, s2, 1));
-return (0);
-}
-else
-{
-s2++;
-return (cmp(s1, s2, 1));
-}
-}
-
 
 /**
  * wildcmp - compares two strings and returns 1 if the strings can be
@@ -59,11 +12,14 @@ return (cmp(s1, s2, 1));
  */
 int wildcmp(char *s1, char *s2)
 {
-int w = 0;
-if (*s2 == '*')
+if (*s1 == '\0' && *s2 == '\0')
+return (1);
+
+if (*s1 == *s2 || *s2 == '*')
 {
-w = 1;
-s2++;
+if (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+return (1);
 }
-return (cmp(s1, s2, w));
+
+return (0);
 }
