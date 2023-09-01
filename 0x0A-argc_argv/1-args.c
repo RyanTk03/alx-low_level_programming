57df2bf91a2bbcb6644785f664f450e41d9cf9d7
RyanTk-L <tkryan03@gmail.com>
2023-09-01 16:35:51 -0700

    0x0A-1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,7 +3,7 @@
 #include "main.h"
 
 /**
- * main - a program that prints its name, followed by a new line.
+ * main - a program that prints the number of arguments passed into it.
  * @argv: The arguments.
  * @argc: The number of argument.
  *
@@ -12,7 +12,7 @@
 int main(int argc,  char *argv[])
 {
 	(void)argc;
-	printf("%s\n", argv[0]);
+	printf("%d\n", argc);
 
 	return (0);
 }
