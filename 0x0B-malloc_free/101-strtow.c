a83d7510cee10747834c5521310a6fbc2575e39e
RyanTk-L <tkryan03@gmail.com>
2023-09-05 16:16:16 -0700

    0x0B-13

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,6 +7,13 @@
 int count_words(char *str);
 char *duplicate_word(char *start, char *end);
 
+/**
+ * strtow - transform string to a list of words
+ *
+ * @str: the string to transform.
+ *
+ * Return: a list of word or null if it fail.
+ */
 char **strtow(char *str)
 {
 int i = 0;
@@ -18,10 +25,26 @@ int word_index = 0;
 char *word_start = str;
 
 if (str == NULL || *str == '\0')
-return NULL;
+return (NULL);
+
+i = 0;
+while (str[i])
+{
+if (!IS_SPACE(str[i]))
+{
+i = -1;
+break;
+}
+i++;
+}
+
+if (i > 0)
+return (NULL);
+
+i = 0;
 
 if (words == NULL)
-return NULL; /* Memory allocation failed */
+return (NULL); /* Memory allocation failed */
 
 while (*str)
 {
@@ -44,7 +67,7 @@ if (words[word_index] == NULL)
 for (i = 0; i < word_index; i++)
 free(words[i]);
 free(words);
-return NULL;
+return (NULL);
 }
 word_index++;
 in_word = 0;
@@ -72,6 +95,13 @@ words[word_index] = NULL; /* Add a NULL pointer at the end of the array */
 return (words);
 }
 
+/**
+ * count_words - count the number of words in a string
+ *
+ * @str: the string to count.
+ *
+ * Return: the number of words in the string.
+ */
 int count_words(char *str)
 {
 int count = 0;
@@ -87,7 +117,7 @@ in_word = 1;
 count++;
 }
 }
-else 
+else
 in_word = 0;
 
 str++;
@@ -96,6 +126,15 @@ str++;
 return (count);
 }
 
+
+/**
+ * duplicate_word - duplicate a word
+ *
+ * @start: the begining of the word to duplicate.
+ * @end: the ending of the word to duplicate.
+ *
+ * Return: the word duplicated.
+ */
 char *duplicate_word(char *start, char *end)
 {
 int word_len = end - start;
@@ -103,7 +142,7 @@ char *word = (char *)malloc((word_len + 1) * sizeof(char));
 int i = 0;
 
 if (word == NULL)
-return NULL; /* Memory allocation failed */
+return (NULL); /* Memory allocation failed */
 
 for (i = 0; i < word_len; i++)
 word[i] = start[i];
