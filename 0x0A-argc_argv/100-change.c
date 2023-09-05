57d5fb10c374075557fe3ad07571978af8167b49
RyanTk-L <tkryan03@gmail.com>
2023-09-05 13:13:39 -0700

    0x0A-12

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,46 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "main.h"
-
 /**
- * main - a program that prints the minimum number of coins to make change for
- *	an amount of money.
- * @argc: The number of arguments.
- * @argv: The value of arguments.
+ * main - prints minimum number coins to make change for an amount of money.
+ * @argc: number of arguments
+ * @argv: array with the arguments
  *
- * Return: 1 on error, 0 else.
- */
+ * Return: always 0
+ **/
+
 int main(int argc, char *argv[])
 {
-int i = 0;
-int count = 0;
-int coins[] = {25, 10, 5, 2, 1};
-int num_coins = sizeof(coins) / sizeof(coins[0]);
-int cents = atoi(argv[1]);
+	int dev = 0, coins = 0;
+	char c[] = "Error";
 
-if (argc != 2)
-{
-printf("Error\n");
-return (1);
-}
-
-if (cents < 0)
-{
-printf("0\n");
-return (0);
-}
-
-for (i = 0; i < num_coins; i++)
-{
-while (cents >= coins[i])
-{
-cents -= coins[i];
-count++;
-}
-}
-
-printf("%i\n", count);
-
-return (0);
+	if (argc != 2)
+	{
+		printf("%s\n", c);
+		return (1);
+	}
+	dev = atoi(argv[1]);
+	while (dev >= 25)
+	{
+		dev -= 25;
+		coins++;
+	}
+	while (dev >= 10)
+	{
+		dev -= 10;
+		coins++;
+	}
+	while (dev >= 5)
+	{
+		dev -= 5;
+		coins++;
+	}
+	while (dev >= 2)
+	{
+		dev -= 2;
+		coins++;
+	}
+	if (dev == 1)
+		coins++;
+	printf("%d\n", coins);
+	return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,9 +12,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
+	int result = 0;
 
 	if (argc != 3)
 	{
@@ -22,7 +20,9 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	printf("%i\n", result);
+	result = atoi(argv[1]) * atoi(argv[2]);
+
+	printf("%d\n", result);
 
 	return (0);
 }
