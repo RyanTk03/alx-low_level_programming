cc72e72b076fb4a8e52403df0acd12213e1310a9
Rayane <rayanetoko@gmail.com>
2023-11-08 10:19:45 -0800

    0x07-1

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -8,7 +8,9 @@
  */
 void print_opcodes(unsigned char *main_address, int num_bytes)
 {
-	for (int i = 0; i < num_bytes; i++)
+	int i = 0;
+
+	for (i = 0; i < num_bytes; i++)
 	{
         	printf("%02x", main_address[i]);
 		if (i < num_bytes - 1)
@@ -28,20 +30,24 @@ void print_opcodes(unsigned char *main_address, int num_bytes)
  */
 int main(int argc, char *argv[])
 {
+	int num_bytes;
+	unsigned char *main_address;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
 
-	int num_bytes = atoi(argv[1]);
+	num_bytes = atoi(argv[1]);
 
-	if (num_bytes < 0) {
+	if (num_bytes < 0)
+	{
 		printf("Error\n");
 		return (2);
 	}
 
-	unsigned char *main_address = (unsigned char *)main;
+	main_address = (unsigned char *)main;
 	print_opcodes(main_address, num_bytes);
 
 	return (0);
