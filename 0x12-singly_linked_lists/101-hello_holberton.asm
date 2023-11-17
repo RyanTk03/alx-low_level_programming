1539c4ee35f2fd86ed2408c74ea4e51b103a8952
Rayane <rayanetoko@gmail.com>
2023-11-17 12:34:05 -0800

    0x12-6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,6 @@
 section .data
     hello db "Hello, Holberton", 0
+    format db "%s", 0
 
 section .text
     global main
@@ -8,10 +9,11 @@ section .text
 
 main:
     ; Prepare the arguments for printf
-    mov rdi, hello  ; Format string
-    call printf     ; Call printf
+    mov rdi, format  ; Format string
+    mov rsi, hello   ; Pointer to the string
+    call printf      ; Call printf
 
     ; Exit the program
-    mov rax, 60      ; syscall: exit
-    xor rdi, rdi     ; status: 0
+    mov rax, 60       ; syscall: exit
+    xor rdi, rdi      ; status: 0
     syscall
