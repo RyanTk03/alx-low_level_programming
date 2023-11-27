92a8d8015eb2bfc476252469f3dfc8b94f69e10d
Rayane <rayanetoko@gmail.com>
2023-11-27 14:53:37 -0800

    0x15-3

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,28 +1,27 @@
 #include "main.h"
 #include <unistd.h>
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 
 /**
- * create_file - function with two arguments
- * @filename: pointer to string name
- * @text_content: pointer to string contents
+ * append_text_to_file - function with two arguments
+ * @filename: pointer to name of file
+ * @text_content: pointer to string content
  *
- * Description: creates a file
- * Return: 1 on success or -1 failure
+ * Description: function that appends to text at the end of a file
+ * Return: 1 on success or -1 for failure
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int count = 0;
-	int fd = 0;
 	int output = 0;
+	int fd = 0;
 
-	if (filename == NULL)
+	if (filename != NULL)
+		fd = open(filename, O_WRONLY | O_APPEND);
+	else
 		return (-1);
-
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -32,15 +31,11 @@ int create_file(const char *filename, char *text_content)
 			count++;
 	}
 	else
-	{
-		close(fd);
 		return (1);
-	}
 
 	output = write(fd, text_content, count);
-	if (output == -1 || output != count)
+	if (output == -1)
 		return (-1);
-
 	close(fd);
 	return (1);
 }
